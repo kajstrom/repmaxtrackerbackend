201e611f1a64a48f490c61ce816d4c3bfaf0b6e7
Kaj Ström <kaj.strom@kapsi.fi>
2017-12-20 19:03:36 +0200

    Allow CORS

--- src/main/java/fi/kajstrom/repmaxtracker/Application.java
+++ src/main/java/fi/kajstrom/repmaxtracker/Application.java
@@ -6,7 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -34,5 +46,18 @@ public class Application implements CommandLineRunner {
         jdbcTemplate.execute("INSERT INTO exercises(name) VALUES('Deadlift')");
         jdbcTemplate.execute("INSERT INTO exercises(name) VALUES('Back Squat')");
         jdbcTemplate.execute("INSERT INTO exercises(name) VALUES('Bench Press')");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowCredentials(true)
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
--- src/main/java/fi/kajstrom/repmaxtracker/resources/ExerciseController.java
+++ src/main/java/fi/kajstrom/repmaxtracker/resources/ExerciseController.java
@@ -2,10 +2,7 @@ package fi.kajstrom.repmaxtracker.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,7 +11,7 @@ import java.util.List;
 public class ExerciseController {
     @Autowired
     JdbcTemplate jdbcTemplate;
-
+    
     @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody List<Exercise> all() {
         return jdbcTemplate.query("SELECT * FROM exercises", (rs, rowNum) -> {
