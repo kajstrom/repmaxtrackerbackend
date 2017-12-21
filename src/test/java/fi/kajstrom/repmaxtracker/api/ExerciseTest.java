c6844bc3b0d4ddd08fee269f0228fe08b47b04ca
Kaj Ström <kaj.strom@kapsi.fi>
2017-12-21 19:54:56 +0200

    Removes Greeting classes used in initial tests

--- src/test/java/fi/kajstrom/repmaxtracker/api/ExerciseTest.java
+++ src/test/java/fi/kajstrom/repmaxtracker/api/ExerciseTest.java
@@ -1,19 +1,12 @@
 package fi.kajstrom.repmaxtracker.api;
 
-import fi.kajstrom.repmaxtracker.resources.AddGreeting;
 import fi.kajstrom.repmaxtracker.resources.Exercise;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
